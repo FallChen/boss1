00e893900b23a60e1af147b85b64c500ff4ae527
cheny <1341948335@qq.com>
2020-05-28 14:15:19 +0800

    一个小型的web服务器 Signed-off-by: cheny <1341948335@qq.com>

--- src/main/java/com/cy/service/MoreRouteService.java
+++ src/main/java/com/cy/service/MoreRouteService.java
@@ -1,10 +1,8 @@
 package com.cy.service;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
-import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.asyncsql.MySQLClient;
 import io.vertx.ext.sql.SQLClient;
@@ -14,6 +12,10 @@ import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +23,7 @@ public class MoreRouteService extends AbstractVerticle {
 
 
     private SQLClient sqlClient;
+//    private FreeMarkerTemplateEngine templateEngine;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MoreRouteService.class);
 
@@ -39,6 +42,10 @@ public class MoreRouteService extends AbstractVerticle {
         Router router = Router.router(vertx);   // <2>
         router.get("/commn").handler(this::autoIncrement);
         router.get("/commn").handler(this::indexHandler);
+        router.get("/html").handler(this::targetHtml);
+        router.route("/js/*").handler(this::jsTest);
+        router.route("/css/*").handler(this::cssTest);
+//        router.get("/js/t.js").handler(this::targetJs);
 
 //        router.get("/").handler(this::indexHandler);
 //        router.get("/wiki/:page").handler(this::pageRenderingHandler); // <3>
@@ -49,8 +56,7 @@ public class MoreRouteService extends AbstractVerticle {
 
 //        templateEngine = FreeMarkerTemplateEngine.create(vertx);
 
-        server
-                .requestHandler(router)   // <5>
+        server.requestHandler(router)   // <5>
                 .listen(8080, ar -> {   // <6>
                     if (ar.succeeded()) {
                         LOGGER.info("HTTP server running on port 8080");
@@ -62,6 +68,66 @@ public class MoreRouteService extends AbstractVerticle {
                 });
 
         return promise.future();
+    }
+
+    private void cssTest(RoutingContext context) {
+        String path = context.request().path();
+        String substring = path.substring(1, path.length());
+        try (InputStream stream = MoreRouteService.class.getClassLoader().getResourceAsStream(substring);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream))) {
+            String temp ="";
+            StringBuilder all = new StringBuilder();
+            while ((temp=bufferedReader.readLine())!=null){
+                all.append(temp+"\r\n");
+            }
+            HttpServerResponse response = context.response();
+            context.response().putHeader("Content-Type", "text/css");
+            response.end(all.toString());
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+            context.fail(e);
+        }
+    }
+
+    private void jsTest(RoutingContext context) {
+        String path = context.request().path();
+        String substring = path.substring(1, path.length());
+        try (InputStream stream = MoreRouteService.class.getClassLoader().getResourceAsStream(substring);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream))) {
+            String temp ="";
+            StringBuilder all = new StringBuilder();
+            while ((temp=bufferedReader.readLine())!=null){
+                all.append(temp+"\r\n");
+            }
+            HttpServerResponse response = context.response();
+            context.response().putHeader("Content-Type", "application/javascript;charset=UTF-8");
+            response.end(all.toString());
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+            context.fail(e);
+        }
+//        context.response().end();
+    }
+
+    private void targetJs(RoutingContext context) {
+
+
+        try (InputStream stream = MoreRouteService.class.getClassLoader().getResourceAsStream("js/t.js");
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream))) {
+            String temp ="";
+            StringBuilder all = new StringBuilder();
+            while ((temp=bufferedReader.readLine())!=null){
+                all.append(temp);
+            }
+            HttpServerResponse response = context.response();
+            context.response().putHeader("Content-Type", "application/javascript;charset=UTF-8");
+            response.end(all.toString()+"\r\n");
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+            context.fail(e);
+        }
+
+
     }
 
     private Future<Void> getConntion() {
@@ -129,6 +195,24 @@ public class MoreRouteService extends AbstractVerticle {
         });
     }
 
+    private void targetHtml(RoutingContext context){
+        try (InputStream stream = MoreRouteService.class.getClassLoader().getResourceAsStream("test.html");
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream))) {
+            String temp ="";
+            StringBuilder all = new StringBuilder();
+            while ((temp=bufferedReader.readLine())!=null){
+                all.append(temp+"\r\n");
+            }
+            HttpServerResponse response = context.response();
+            context.response().putHeader("Content-Type", "text/html");
+            ;
+            response.end(all.toString());
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+            context.fail(e);
+        }
+
+    }
 
     public static void main(String[] args) {
         Vertx.vertx().deployVerticle(MoreRouteService.class.getName());
